d061e4e22ec531287c7008f71352bba039440a1f
CALLMEBOB <sankaapb1@gmail.com>
2023-01-25 13:18:17 +0700

    fixed

--- prisma/migrations/20230125044301_fix/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "holiday_year" (
-    "id" UUID NOT NULL,
-    "date" TIMESTAMP(3) NOT NULL,
-    "holiday_name" VARCHAR(255)
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "holiday_year_id_key" ON "holiday_year"("id");
