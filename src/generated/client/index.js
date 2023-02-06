6afff16a33c8a51c78fc4f9f55a5585e31365793
mpat1234 <Psanorit@gmail.com>
2023-02-06 14:48:21 +0700

    m

--- src/generated/client/index.js
+++ src/generated/client/index.js
@@ -483,7 +483,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "C:\\Users\\MNatc\\OneDrive\\เดสก์ท็อป\\65C_NewHRConnext_Backend\\src\\generated\\client",
+      "value": "C:\\Users\\admin\\Desktop\\ptt_work\\hr\\65C_NewHRConnext_Backend\\src\\generated\\client",
       "fromEnvVar": null
     },
     "config": {
