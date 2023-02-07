7ab23d3bf00d0f84dc7d121c69cf15a46992d4bc
mpat1234 <Psanorit@gmail.com>
2023-02-07 17:55:38 +0700

    m

---
+++ prisma/migrations/20230207103412_/migration.sql
@@ -0,0 +1,2 @@
+-- DropIndex
+DROP INDEX "Position_user_user_id_key";
---
+++ src/datasource/leave.gql.ts
@@ -0,0 +1,29 @@
+import { composeResolvers } from '@graphql-tools/resolvers-composition';
+import { authenticate } from '../middleware/authenticatetoken';
+import { Resolvers } from '../generated/graphql';
+import gql from 'graphql-tag';
+import _ from 'lodash';
+import { v4 } from 'uuid';
+
+export const leaveTypedef = gql`
+type leave_data{
+ id: String            
+ leavetype_id: String            
+ leave_type: leave_type    
+ start_date: Date
+ end_date: Date
+ quantity_day: String
+ detail_leave: String 
+ Status: Int                
+ user_id: String   
+ .         
+ user:  User              
+}
+
+type leave_type{
+  id: String 
+  name: String
+  orderby: Int       
+}
+
+`
