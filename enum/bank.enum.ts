937fa8195f90412ee4d6c304bfa54ee691ad92d5
khaisktl <sakuntala.km@gmail.com>
2023-02-14 13:29:37 +0700

    seed

---
+++ enum/bank.enum.ts
@@ -0,0 +1,67 @@
+export default [
+  {
+    id: '6c49f44a-7921-42b0-bfed-ab838694e4ca',
+    name: 'ธนาคารกรุงเทพ',
+    bank_code: 'BBL',
+  },
+  {
+    id: '044e2a1c-43b3-41e2-a460-be1fee139e22',
+    name: 'ธนาคารกสิกรไทย',
+    bank_code: 'KBANK',
+  },
+  {
+    id: 'b30b62e1-829a-4b8a-9d1d-9f82ea8fafd1',
+    name: 'ธนาคารกรุงไทย',
+    bank_code: 'KTB',
+  },
+  {
+    id: '19541698-97a3-4f4d-a676-89e7b5fe879d',
+    name: 'ธนาคารทหารไทยธนชาติ',
+    bank_code: 'TTB',
+  },
+  {
+    id: '4a54210f-7723-41bd-9f53-aa196f5e5b7e',
+    name: 'ธนาคารกรุงศรีอยุธยา',
+    bank_code: 'BAY',
+  },
+  {
+    id: '23b169fd-5143-4465-bd55-33ae8fb610a4',
+    name: 'ธนาคารเกียรตินาคินภัทร',
+    bank_code: 'KKP',
+  },
+  {
+    id: 'f176256e-17e5-4c02-a600-4e70330e0f34',
+    name: 'ธนาคารซีไอเอ็มบีไทย',
+    bank_code: 'CIMB Thai',
+  },
+  {
+    id: '40d97b8d-fc01-47a8-9654-3a3f9cff984b',
+    name: 'ธนาคารออมสิน',
+    bank_code: 'GSB',
+  },
+  {
+    id: 'd475d0aa-5296-428e-a740-2be0ab27f76e',
+    name: 'ธนาคารยูโอบี',
+    bank_code: 'UOB',
+  },
+  {
+    id: '3b159a42-50e3-4e99-bcd3-f346fcef3b9b',
+    name: 'ธนาคารอิสลามแห่งประเทศไทย',
+    bank_code: 'iBank',
+  },
+  {
+    id: '40391bc3-8374-475f-a480-29ecc25b0d20',
+    name: 'ธนาคารเพื่อการเกษตรและสหกรณ์การเกษตร',
+    bank_code: 'BAAC',
+  },
+  {
+    id: 'a2c2bf27-0e4d-442c-9ed5-24e917f564b5',
+    name: 'ธนาคารอาคารสงเคราะห์',
+    bank_code: 'GHB',
+  },
+  {
+    id: '3dea9546-efbb-498a-81de-aa7f03c20b17',
+    name: 'ธนาคารซิตี้แบงค์',
+    bank_code: 'CITI',
+  },
+]
