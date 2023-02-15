3574e07ff80df3bc51f42d4746a9c5071ad5dc15
khaisktl <sakuntala.km@gmail.com>
2023-02-15 12:30:53 +0700

    holiday

--- prisma/migrations/20230214205829_/migration.sql
+++ prisma/migrations/20230214205829_/migration.sql
@@ -1,17 +1,1 @@
-/*
-  Warnings:
 
-  - You are about to drop the `holidayCompany` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropForeignKey
-ALTER TABLE "holidayCompany" DROP CONSTRAINT "holidayCompany_CompanyId_fkey";
-
--- DropForeignKey
-ALTER TABLE "holidayCompany" DROP CONSTRAINT "holidayCompany_holiday_dateId_fkey";
-
--- DropForeignKey
-ALTER TABLE "holidayCompany" DROP CONSTRAINT "holidayCompany_holiday_yearId_fkey";
-
--- DropTable
-DROP TABLE "holidayCompany";
